bdd83dcdb92d5518b8a45c810b464efc3ba34344
Keval <keval.langalia@live.com>
2021-02-02 06:37:01 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/sqlite_for_dbdesign/MainActivity.java
+++ app/src/main/java/com/example/sqlite_for_dbdesign/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.sqlite_for_dbdesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        // Inserting Contacts
+        Log.d("Insert: ", "Inserting ..");
+        db.addContact(new Contact("Ravi", "9100000000"));
+        db.addContact(new Contact("Srinivas", "9199999999"));
+        db.addContact(new Contact("Tommy", "9522222222"));
+        db.addContact(new Contact("Karthik", "9533333333"));
+
+        // Reading all contacts
+        Log.d("Reading: ", "Reading all contacts..");
+        List<Contact> contacts = db.getAllContacts();
+
+        for (Contact cn : contacts) {
+            String log = "Id: " + cn.getID() + " ,Name: " + cn.getName() + " ,Phone: " +
+                    cn.getPhoneNumber();
+            // Writing Contacts to log
+            Log.d("Name: ", log);
+        }
+
+
+        Log.d("Database path: ", getDatabasePath("contactsManager").getAbsolutePath());
+
     }
+
 }
